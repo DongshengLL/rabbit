3bd51226aa98f75b1ccc57579b939f0ac8109c9c
Janus <598595404@qq.com>
2023-01-12 16:18:19 +0800

    222

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,22 @@
-import { defineConfig } from 'vite'
+import {
+    defineConfig
+} from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from "path"
+import {
+    resolve
+} from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"./",
-  server: {
-    host: '0.0.0.0',
-    open: false,
-  },
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "src")
-    }
-  },
-  plugins: [vue()],
+    base: "./",
+    server: {
+        host: '0.0.0.0',
+        open: false,
+    },
+    resolve: {
+        alias: {
+            "@": resolve(__dirname, "src")
+        }
+    },
+    plugins: [vue()],
 })
