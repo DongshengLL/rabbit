c347770f17036d75ff767b64fa47053caf3a3214
Janus <598595404@qq.com>
2023-01-12 15:59:41 +0800

    test

--- src/main.js
+++ src/main.js
@@ -1,10 +1,24 @@
-import { createApp } from 'vue'
+/*
+ * @Author: Janus 598595404@qq.com
+ * @Date: 2023-01-12 14:44:06
+ * @LastEditors: Janus 598595404@qq.com
+ * @LastEditTime: 2023-01-12 15:59:13
+ * @FilePath: \rabbit\src\main.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import {
+    createApp
+} from 'vue'
 import './style.scss'
 import App from './App.vue'
 
-import { createPinia } from "pinia";
+import {
+    createPinia
+} from "pinia";
 const pinia = createPinia();
 
+console.log(1122)
+console.log(pinia)
 
 const app = createApp(App);
 app.use(pinia);
