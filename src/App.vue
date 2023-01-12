0487f31185791c8483d410105a1aa723cd9890f6
Janus <598595404@qq.com>
2023-01-12 14:44:43 +0800

    add: project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import PressScene from "./view/PressScene.vue"
+import MainScene from "./view/MainScene.vue"
+import { useSystemStore } from "@/store/system";
+const store = useSystemStore();
+</script>
+
+<template>
+    <press-scene v-if="store.scene == 'press'" />
+    <main-scene v-if="store.scene == 'play'" />
+</template>
+
+<style lang="scss" scoped>
+
+</style>
